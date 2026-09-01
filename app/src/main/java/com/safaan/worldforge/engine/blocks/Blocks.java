d0f1a8f036c8116cfc4ac962522a4b1f4e2aa89a
Safaan5559 <safaan865@gmail.com>
2026-09-01 20:28:32 +0530

    Add Phase 1 block registry

---
+++ app/src/main/java/com/safaan/worldforge/engine/blocks/Blocks.java
@@ -0,0 +1,9 @@
+package com.safaan.worldforge.engine.blocks;
+
+public final class Blocks {
+    private Blocks() {}
+    public static final Block[] HOTBAR = {
+        Block.GRASS, Block.DIRT, Block.STONE, Block.SAND,
+        Block.WATER, Block.WOOD, Block.LEAVES, Block.STONE
+    };
+}
